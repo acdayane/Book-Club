fc42e5040be6b16e7458aa87ca981a64b139a04e
Dayane <acdayane@gmail.com>
2023-12-16 22:17:01 -0300

    test: unit user service

--- src/test/java/ada/mod3/bookclub/service/UserServiceUnitTest.java
+++ src/test/java/ada/mod3/bookclub/service/UserServiceUnitTest.java
@@ -1,15 +1,17 @@
 package ada.mod3.bookclub.service;
 
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import ada.mod3.bookclub.controller.dto.UserRequest;
+import ada.mod3.bookclub.model.User;
 import ada.mod3.bookclub.repository.UserRepository;
 
 @ExtendWith(SpringExtension.class)
@@ -24,13 +26,29 @@ public class UserServiceUnitTest {
     @InjectMocks
     private UserService userService;
 
-    // @Test
-    // public void Should_CheckUser() {
-    //     UserRequest userDTO = new UserRequest("int-test", "int@test.com", "int-test");
-    //     //userController.saveUser(userDTO);
-    //     passwordEncoder = new BCryptPasswordEncoder();
+    @Test
+    public void Should_Authenticate_When_UserExists() {
+        User user = new User();
+        user.setEmail("unit@test.com");
 
-    //     userService = new UserService(userRepository, passwordEncoder);
+        when(userRepository.findByEmail("unit@test.com")).thenReturn(user);
+    }
 
-    // }
+    @Test
+    public void Should_ThrowsException_When_InvalidUser() {
+        User user = new User();
+        user.setEmail("unit@test.com");
+
+        User userResponse = userRepository.findByEmail("wrong@test.com");
+
+        if (userResponse == null) {
+            Assertions.assertThrows(IllegalArgumentException.class, new Executable() {
+                @Override
+                public void execute() throws Throwable {
+                    throw new IllegalArgumentException("User not found");
+                }
+            });             
+        }
+    }
+
 }
