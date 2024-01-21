48e5387543f763940ec97be337ce203221139ac1
Dayane <acdayane@gmail.com>
2024-01-21 18:42:43 -0300

    fix: user service unit test

--- src/test/java/ada/mod3/bookclub/service/UserServiceUnitTest.java
+++ src/test/java/ada/mod3/bookclub/service/UserServiceUnitTest.java
@@ -5,7 +5,6 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,28 +26,34 @@ public class UserServiceUnitTest {
     private UserService userService;
 
     @Test
-    public void Should_Authenticate_When_UserExists() {
+    public void given_user_exists_when_search_by_email_then_user_should_returns() {
+        //Criação dos dados utilizados.
         User user = new User();
+        user.setId(1);
         user.setEmail("unit@test.com");
 
         when(userRepository.findByEmail("unit@test.com")).thenReturn(user);
+
+        //Chamada da lógica para execução do código a ser testado
+        var found = userService.getUserByEmail("unit@test.com");
+
+        //Validações do resultado
+        Assertions.assertEquals(user.getId(), found.getId());
+        Assertions.assertEquals(user.getEmail(), found.getEmail());
     }
 
     @Test
-    public void Should_ThrowsException_When_InvalidUser() {
+    public void given_user_was_not_registered_when_search_by_email_then_exception_should_thrown() {
+        //Criação dos dados utilizados.
         User user = new User();
         user.setEmail("unit@test.com");
 
-        User userResponse = userRepository.findByEmail("wrong@test.com");
-
-        if (userResponse == null) {
-            Assertions.assertThrows(IllegalArgumentException.class, new Executable() {
-                @Override
-                public void execute() throws Throwable {
-                    throw new IllegalArgumentException("User not found");
-                }
-            });             
-        }
+        //Validações da execução da parte lógica. O código dentro do assertThrows será executado primeiro.
+        Assertions.assertThrows(
+                RuntimeException.class,
+                //Chamada da lógica para exeucução do código a ser testado
+                () -> userService.getUserByEmail("wrong@test.com")
+        );
     }
 
 }
